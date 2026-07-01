d78aa50918aa242548ae3b284f49d104d8b06f3d
AstraQuarkX <astraquark1@gmail.com>
2026-07-01 23:03:27 +0530

    Refactor variable initialization and print statements
    Updated the video variable to use octal notation and modified printf statements to separate print calls for clarity.

--- C/V002_C_Variables_Basic_Datatypes.c
+++ C/V002_C_Variables_Basic_Datatypes.c
@@ -4,10 +4,14 @@ int main(){
     
     char name[] = "AstraQuark";      // String - Double Quotes
     char score = 'X';                // Character - Single Quotes
-    int video = 2;                   // Integer
+    int video = 02;                   // Integer
     float version = 0.100;           // Float (Decimal)
     double code = 99.9876543210;     // Double (Decimal)
-    printf("Name : %s \nScore : %c \nVid No. : %d %i \nVersion : %.2f %.1F \nCode : %.10lf",name,score,video,video,version,version,code);
+    print("Name : %s", name);
+    print("%c \n", score);
+    print("Video : %d \n", video);
+    print("Version : v%.1f \n", version);
+    print("Code : %.10lf", code);
     
     return 0;
 }

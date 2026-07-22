11bff52afd8a75b7ccc246720120659af54bd499
AstraQuarkX <astraquark1@gmail.com>
2026-07-22 22:11:29 +0530

    Clarify comment on stripping whitespace from strings

--- variables_&_datatypes.py
+++ variables_&_datatypes.py
@@ -13,7 +13,7 @@ print("\"Hello World\"")   # \" inserts " : "Hello World"
 # DOUBT print("Hello\rWorld")
 print("Hello\bWorld")      # Backspace - Deletes 'o'
 
-# Stripping Whitespace :
+# Stripping Whitespace from Strings :
 print(name.rstrip())       # Removes the whitespace from right end temporarily 
 print(name.lstrip())       # Removes the whitespace from left end temporarily 
 print(name.strip())        # Removes the whitespace from both ends temporarily 

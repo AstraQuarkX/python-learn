8fdc9b840c7ef0b56953e650ac337a5c43ecc5db
AstraQuarkX <astraquark1@gmail.com>
2026-07-22 22:56:10 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,8 +1,16 @@
-# List :
+# Adding Items to  List :
 brand = ['Quark']
 brand.append('X')
 brand.insert(0,'Astra')  # list.insert(index,value)
 brand.extend([3,4,5])    # brand = ['Astra','Quark','X',3,4,5]
-brand.extend('xy')       # num = ['Astra','Quark','X',3,4,5,'x','y']
+brand.extend('xy')       # brand = ['Astra','Quark','X',3,4,5,'x','y']
 
-del brand[2]  # del list[index]
+# Deleting Items from List :
+del brand[1]  # del list[index]
+     # brand = ['Astra','X',3,4,5,'x','y']
+popped_value = brand.pop(1)   # popped_val = list.pop(index) {pop() returns the popped value}
+     # brand = ['Astra',3,4,5,'x','y']
+     # popped_value = 'X'
+brand.remove('x')  # list.remove(value)
+     # brand = ['Astra',3,4,5,'y']
+     # It removes only the first occurrence of the value
